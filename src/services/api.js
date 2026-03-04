20d5a73a3dc49d2a81933c24787c7330643ec491
sanjus <sanjuskumarkply@gmail.com>
2026-03-04 15:18:00 +0530

    edit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// const API_BASE = "http://127.0.0.1:8000/api";
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 // ====================== API FETCH WITH AUTO REFRESH ======================
